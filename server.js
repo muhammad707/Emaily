88e419d6abca09f72e114d0d4065656a479cca9c
MSM_707 <sirojiddinov.1881@mail.ru>
2018-09-21 12:12:02 +0500

    Final commit

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const keys = require('./config/keys');
+const keys = require('./server/config/keys');
 const express = require('express');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const path =  require('path');
 
-const { User } = require('./models/user');
-require('./services/passport');
-const mainRoutes = require('./routes/mainRoutes');
-const authRoutes = require('./routes/authRoutes');
-const billingRoutes  = require('./routes/billingRoutes');
-const { mongoose } = require('./db/mongodb'); // object destructuring that only stores mongoose property
+const { User } = require('./server/models/user');
+require('./server/services/passport');
+// const mainRoutes = require('./server/routes/mainRoutes');
+const authRoutes = require('./server/routes/authRoutes');
+const billingRoutes  = require('./server/routes/billingRoutes');
+const { mongoose } = require('./server/db/mongodb'); // object destructuring that only stores mongoose property
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(bodyParser.json());
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
 
 	const path = require('path');
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 		// console.log(path.join(__dirname, './../client', 'build', 'index.html'));
 	});
 } 
